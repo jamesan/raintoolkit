ebd4cd05252893a0f4a4b10f07cf320001d45a22
James An <james@jamesan.ca>
2016-12-01 16:21:37 -0500

    Revert "reorganizing branches" by removing trunk.old folder.
    This reverts commit f309728a2100cbbc7cb9a440488ed28c774075d6.

--- trunk.old/simplejpa-src/com/spaceprogram/simplejpa/stats/Statistics.java
+++
@@ -1,9 +0,0 @@
-package com.spaceprogram.simplejpa.stats;
-
-/**
- * User: treeder
- * Date: Oct 9, 2008
- * Time: 6:07:28 PM
- */
-public interface Statistics {
-}
--- trunk.old/src/rain/authorization/ActionNotAuthorizedException.java
+++
@@ -1,9 +0,0 @@
-/*
- * Created on Nov 20, 2008
- *
- */
-package rain.authorization;
-
-public class ActionNotAuthorizedException extends Exception {
-
-}
